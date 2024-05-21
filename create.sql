c1f62a65768adcedc5b160619330502d7ed06354
aviv15s <aviv.shemesh.43@gmail.com>
2024-05-21 12:02:45 +0300

    upload

---
+++ create.sql
@@ -0,0 +1,17 @@
+create table oscars(
+  id varchar,
+  film_name varchar,
+  oscar_year varchar,
+  studio varchar,
+  award varchar,
+  release_year varchar,
+  duration varchar,
+  genres varchar,
+  imdb_rating varchar,
+  imdb_votes varchar,
+  content_rating varchar,
+  directors varchar,
+  authors varchar,
+  actors varchar,
+  filmId varchar
+);
