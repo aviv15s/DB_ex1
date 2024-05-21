e50e4f8ab349ea7ca22f2221e69d16686a990a43
aviv15s <aviv.shemesh.43@gmail.com>
2024-05-21 16:44:35 +0300

    upload

--- create.sql
+++ create.sql
@@ -1,17 +1,45 @@
 create table oscars(
-  id varchar,
-  film_name varchar,
-  oscar_year varchar,
-  studio varchar,
-  award varchar,
-  release_year varchar,
-  duration varchar,
-  genres varchar,
-  imdb_rating varchar,
-  imdb_votes varchar,
-  content_rating varchar,
-  directors varchar,
-  authors varchar,
-  actors varchar,
-  filmId varchar
+  id INTEGER PRIMARY KEY,
+  film_name varchar(100) NOT NULL,
+  oscar_year INTEGER NOT NULL,
+  studio varchar(100) NOT NULL,
+  award varchar(100) NOT NULL CHECK ( award="Winner" or award="Nominee" ),
+  release_year INTEGER NOT NULL ,
+  duration INTEGER NOT NULL ,
+  imdb_rating FLOAT NOT NULL ,
+  imdb_votes INTEGER NOT NULL ,
+  content_rating varchar(100),
+  filmId INTEGER NOT NULL UNIQUE
+);
+
+create table participants(
+  name varchar(100) PRIMARY KEY,
+);
+
+create table genre_of_movie(
+  genres varchar(100) NOT NULL,
+  movie_name varchar(100) NOT NULL,
+  FOREIGN KEY (movie_name) REFERENCES oscars(movie_name),
+  UNIQUE (genres, movie_name)
+);
+
+create table director_of_movie(
+  name varchar(100) NOT NULL,
+  movie_name varchar(100) NOT NULL,
+  FOREIGN KEY (name) REFERENCES participants(name),
+  FOREIGN KEY (movie_name) REFERENCES oscars(movie_name)
+);
+
+create table actor_in_movie(
+  name varchar(100) NOT NULL,
+  movie_name varchar(100) NOT NULL,
+  FOREIGN KEY (name) REFERENCES participants(name),
+  FOREIGN KEY (movie_name) REFERENCES oscars(movie_name)
+);
+
+create table author_of_movie(
+  name varchar(100) NOT NULL,
+  movie_name varchar(100) NOT NULL,
+  FOREIGN KEY (name) REFERENCES participants(name),
+  FOREIGN KEY (movie_name) REFERENCES oscars(movie_name)
 );
--- ex1.py
+++ ex1.py
@@ -2,28 +2,79 @@ import csv
 from io import TextIOWrapper
 from zipfile import ZipFile
 
-# opens file.
-oscars_outfile = open("oscars.csv", 'w' , encoding='UTF8',newline='')
-oscars_outwriter = csv.writer(oscars_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
-
-
 # process_file goes over all rows in original csv file, and sends each row to process_row()
 def process_file():
-  with ZipFile('oscars.zip') as zf:
-      with zf.open('oscars.csv', 'r') as infile:
-          reader = csv.reader(TextIOWrapper(infile, 'utf-8'))
-          for row in reader:
-              # TO DO splits row into the different csv table files
-              oscars_outwriter.writerow(row)
-  oscars_outfile.close()
+    oscars_outfile = open("oscars.csv", 'w', encoding='UTF8', newline='')
+    oscars_outwriter = csv.writer(oscars_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    participants_outfile = open("participants.csv", 'w', encoding='UTF8', newline='')
+    participants_outwriter = csv.writer(participants_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    actors_outfile = open("actor_in_movie.csv", 'w', encoding='UTF8', newline='')
+    actors_outwriter = csv.writer(actors_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    authors_outfile = open("author_of_movie.csv", 'w', encoding='UTF8', newline='')
+    authors_outwriter = csv.writer(authors_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    directors_outfile = open("director_of_movie.csv", 'w', encoding='UTF8', newline='')
+    directors_outwriter = csv.writer(directors_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    genre_of_movie_outfile = open("genre_of_movie.csv", 'w', encoding='UTF8', newline='')
+    genre_of_movie_outwriter = csv.writer(genre_of_movie_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+    with ZipFile('oscars.zip') as zf:
+        with zf.open('oscars.csv', 'r') as infile:
+            reader = csv.reader(TextIOWrapper(infile, 'utf-8'))
+            for row in reader:
+                id, film_name, oscar_year, studio, award, release, length, genres, IMDB_rating, IMDB_votes, rating, directors, authors, actors, filmID = row
+                # create new movie entry
+                oscars_outwriter.writerow([id, film_name, oscar_year, studio, award, release, length, IMDB_rating, IMDB_votes, rating, filmID])
+
+                authors = split_list_value(authors)
+                actors = split_list_value(actors)
+                directors = split_list_value(directors)
+
+                # add directors, actors and authors
+                # assume film name is not null, only check if actor/director/author name is null
+                seen_names = set()
+
+                for name in authors:
+                    if name != "" and name not in seen_names:
+                        authors_outwriter.writerow([name, film_name])
+                        participants_outwriter.writerow([name])
+                        seen_names.add(name)
+                for name in directors:
+                    if name != "" and name not in seen_names:
+                        directors_outwriter.writerow([name, film_name])
+                        participants_outwriter.writerow([name])
+                        seen_names.add(name)
+                for name in actors:
+                    if name != "" and name not in seen_names:
+                        actors_outwriter.writerow([name, film_name])
+                        participants_outwriter.writerow([name])
+                        seen_names.add(name)
+
+                # add genres
+                genres = split_list_value(genres)
+                for g in genres:
+                    genre_of_movie_outwriter.writerow([g, film_name])
+
+    actors_outfile.close()
+    directors_outfile.close()
+    authors_outfile.close()
+    genre_of_movie_outfile.close()
+    participants_outfile.close()
+    oscars_outfile.close()
 
 # return the list of all tables
 def get_names():
-    return ["oscars"]
+    return ["actor_in_movie", "author_of_movie", "director_of_movie", "genre_of_movie"]
+
 
 def split_list_value(list_value):
     return list_value.split("&&")
-	
+
+
 if __name__ == "__main__":
     process_file()
 
