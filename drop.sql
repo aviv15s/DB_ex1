e73b1b8da5804bace3a1efa9f4432b9239e6ead5
aviv15s <aviv.shemesh.43@gmail.com>
2024-05-22 21:22:25 +0300

    upload

--- drop.sql
+++ drop.sql
@@ -1,7 +1,7 @@
-drop table participants;
 drop table genre_of_movie;
 drop table director_of_movie;
 drop table actor_in_movie;
 drop table author_of_movie;
+drop table participants;
 drop table oscars;
 
